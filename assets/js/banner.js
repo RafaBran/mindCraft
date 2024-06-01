45bad374d3311f0246e3eeac8783648809786121
Rafael Brandão Neves <bnrafael@outlook.com>
2024-06-01 18:59:39 -0300

    Primeiro commit

---
+++ assets/js/banner.js
@@ -0,0 +1,223 @@
+
+function fred() {
+    const conteudoContainner = document.querySelector(".main__conteudo-principal_banner");
+    conteudoContainner.innerHTML = `
+    <h1 style="
+        margin:2rem 3rem 0.3rem 3rem;
+
+
+    ">Fred Simmons Keller foi um psicólogo e educador americano, nascido em 2 de janeiro de 1899 e falecido em 2 de fevereiro de 1996. Ele é amplamente reconhecido por suas contribuições significativas ao campo da psicologia da educação, especialmente pelo desenvolvimento do Sistema Personalizado de Instrução (Personalized System of Instruction – PSI), também conhecido como Método Keller.</h1>
+    `;}
+
+    function psi() {
+    const conteudoContainner = document.querySelector(".main__conteudo-principal_banner");
+    conteudoContainner.innerHTML = `
+    <h1 style="
+    margin:2rem 3rem 0.3rem 3rem;
+
+        
+    ">O Sistema Personalizado de Instrução (Personalized System of Instruction – PSI), também conhecido como Método Keller, é uma abordagem educacional desenvolvida pelo psicólogo Fred S. Keller na década de 1960. Esse método se destaca por sua ênfase no aprendizado individualizado e autônomo, permitindo que os alunos progridam em seu próprio ritmo. 
+    </h1>
+    `;}
+
+    
+
+function principios() {
+    const conteudoContainner = document.querySelector(".main__conteudo-principal_banner");
+    conteudoContainner.innerHTML = `
+    
+    <h1 style="
+        font-size:1.5rem;
+        text-align:left;
+        font-weight: 700;
+        font-weight: 700;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">Progresso Individualizado</h1>
+    
+    <h1 style="
+        font-size:1rem;
+        text-align:justify;
+        margin-bottom:1rem;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">Os alunos avançam no curso conforme sua própria velocidade, sem a pressão de acompanhar uma turma. Isso permite que cada estudante dedique o tempo necessário para dominar cada unidade de conteúdo.</h1>
+    
+    <h1 style="
+        font-size:1.5rem;
+        text-align:left;
+        font-weight: 700;
+        font-weight: 700;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">Materiais de Estudo:</h1>
+    
+    <h1 style="
+        font-size:1rem;
+        text-align:justify;
+        margin-bottom:1rem;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">O curso é estruturado em unidades pequenas e específicas, cada uma contendo materiais de estudo claramente definidos. Esses materiais podem incluir textos, vídeos, exercícios práticos e outros recursos.</h1>
+
+     <h1 style="
+        font-size:1.5rem;
+        text-align:left;
+        font-weight: 700;
+        font-weight: 700;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">Avaliação Frequente:</h1>
+    
+    <h1 style="
+        font-size:1rem;
+        text-align:justify;
+        margin-bottom:1rem;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">Ao final de cada unidade, os alunos realizam uma avaliação para demonstrar sua compreensão do material. Essas avaliações são fundamentais para garantir que os estudantes tenham realmente assimilado o conteúdo antes de avançar.</h1>
+
+    <h1 style="
+    font-size:1.5rem;
+    text-align:left;
+    font-weight: 700;
+    font-weight: 700;
+    padding:1rem 3rem 0.3rem 3rem;
+
+    ">Feedback Imediato:</h1>
+
+    <h1 style="
+    font-size:1rem;
+    text-align:justify;
+    margin-bottom:1rem;
+    padding:1rem 3rem 0.3rem 3rem;
+
+    ">Os alunos recebem feedback imediato sobre suas avaliações, o que facilita a correção de erros e o aprimoramento contínuo.</h1>
+
+    <h1 style="
+    font-size:1.5rem;
+    text-align:left;
+    font-weight: 700;
+    font-weight: 700;
+    padding:1rem 3rem 0.3rem 3rem;
+
+    ">Tutoria:</h1>
+
+    <h1 style="
+    font-size:1rem;
+    text-align:justify;
+    margin-bottom:1rem;
+    padding:1rem 3rem 0.3rem 3rem;
+
+    ">O método Keller frequentemente utiliza tutores (outros estudantes avançados ou professores) para fornecer suporte e orientação individualizada. Esses tutores ajudam a esclarecer dúvidas e oferecem feedback adicional.</h1>
+
+    <h1 style="
+    font-size:1.5rem;
+    text-align:left;
+    font-weight: 700;
+    font-weight: 700;
+    padding:1rem 3rem 0.3rem 3rem;
+
+    ">Ênfase na Maestria:</h1>
+
+    <h1 style="
+    font-size:1rem;
+    text-align:justify;
+    margin-bottom:1rem;
+    padding:1rem 3rem 0.3rem 3rem;
+
+    ">O objetivo é que os alunos alcancem um nível de maestria em cada unidade antes de prosseguir para a próxima. Isso contrasta com métodos tradicionais, onde os alunos podem avançar sem ter dominado completamente o material anterior.</h1>
+`;
+}
+
+function filosofia() {
+    const conteudoContainner = document.querySelector(".main__conteudo-principal_banner");
+    conteudoContainner.innerHTML = `
+    <h1 style="
+    margin:2rem 3rem 0.3rem 3rem;
+
+        
+    ">A filosofia educacional de Fred S. Keller é profundamente enraizada nos princípios do behaviorismo, uma teoria da aprendizagem que enfatiza o papel dos comportamentos observáveis e o impacto do ambiente no aprendizado. 
+    </h1>
+    
+    <h1 style="
+    margin:2rem 3rem 0.3rem 3rem;
+
+        
+    ">Keller acreditava que todos os alunos poderiam alcançar um alto nível de competência se lhes fosse dado tempo suficiente e os recursos apropriados. Ele defendia que a educação deveria ser um processo ativo e envolvente, com ênfase na individualização do ensino para atender às necessidades e aos ritmos de aprendizado de cada aluno.
+    </h1>
+
+    `;}
+
+    function vantagens() {
+        const conteudoContainner = document.querySelector(".main__conteudo-principal_banner");
+        conteudoContainner.innerHTML = `
+        <h1 style="
+        font-size:1.5rem;
+        text-align:left;
+        margin-bottom:1rem;
+        font-weight: 700;
+        font-weight: 700;
+        padding:1rem 3rem 0.3rem 3rem;
+
+
+    ">Autonomia do Aluno:</h1>
+    
+    <h1 style="
+        font-size:1rem;
+        text-align:justify;
+        margin-bottom:1rem;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">O sistema incentiva a autodisciplina e a autonomia, habilidades valiosas não só no contexto educacional, mas também na vida pessoal e profissional.</h1>
+        
+    <h1 style="
+        font-size:1.5rem;
+        text-align:left;
+        margin-bottom:1rem;
+        font-weight: 700;
+        font-weight: 700;
+        padding:1rem 3rem 0.3rem 3rem;
+    ">Redução da Ansiedade:</h1>
+    
+    <h1 style="
+        font-size:1rem;
+        text-align:justify;
+        margin-bottom:1rem;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">Como os alunos não são pressionados a acompanhar um ritmo imposto, eles podem estudar de forma mais relaxada e eficaz.</h1>
+    
+    <h1 style="
+        font-size:1.5rem;
+        text-align:left;
+        margin-bottom:1rem;
+        font-weight: 700;
+        font-weight: 700;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">Personalização do Aprendizado:</h1>
+    
+    <h1 style="
+        font-size:1rem;
+        text-align:justify;
+        margin-bottom:1rem;
+        padding:1rem 3rem 0.3rem 3rem;
+
+    ">O método se adapta às necessidades individuais dos alunos, permitindo um aprendizado mais profundo e personalizado.</h1>
+    
+    
+    `;}
+
+    function avaliacao() {
+        const conteudoContainner = document.querySelector(".main__conteudo-principal_banner");
+        conteudoContainner.innerHTML = `
+        <h1 style="
+        margin:2rem 3rem 0 3rem;
+                    
+        ">	No PSI, as avaliações são utilizadas como ferramentas de aprendizado, não apenas como medidas de desempenho. As avaliações frequentes permitem que os alunos e instrutores identifiquem áreas de dificuldade e intervenham prontamente, garantindo que os alunos permaneçam no caminho certo.
+        </h1>
+        `;}
+
+
+
---
+++ assets/js/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+
+    direction: 'horizontal',
+    loop: false,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+      
+  });
