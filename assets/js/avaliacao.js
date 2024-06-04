84acf543d1b121c8efdf4298992ecd83a983e7ab
Rafael Brandão Neves <bnrafael@outlook.com>
2024-06-04 17:00:03 -0300

    atuaização 04-06-2024 16:59

---
+++ assets/js/avaliacao.js
@@ -0,0 +1,160 @@
+
+function letraA1() {
+    const feedback = document.querySelector('.feedback1');
+    const containnerFeedback = document.querySelector('.containner-feedback1');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Ribossomo: Errado. Os ribossomos são responsáveis pela síntese de proteínas, não pela produção de energia.`;
+}
+
+function letraB1() {
+    const feedback = document.querySelector('.feedback1');
+    const containnerFeedback = document.querySelector('.containner-feedback1');
+    
+    containnerFeedback.style.backgroundColor = '#6dffb6';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Mitocôndria: Certo. A mitocôndria é a organela responsável pela produção de energia na célula, através do processo de respiração celular.`;
+}
+
+function letraC1() {
+    const feedback = document.querySelector('.feedback1');
+    const containnerFeedback = document.querySelector('.containner-feedback1');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Núcleo: Errado. O núcleo é responsável por armazenar o material genético e controlar as atividades celulares.`;
+}
+
+function letraD1() {
+    const feedback = document.querySelector('.feedback1');
+    const containnerFeedback = document.querySelector('.containner-feedback1');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Lisossomo: Errado. Os lisossomos são responsáveis pela digestão intracelular.`;
+}
+
+function letraE1() {
+    const feedback = document.querySelector('.feedback1');
+    const containnerFeedback = document.querySelector('.containner-feedback1');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Retículo Endoplasmático Rugoso: Errado. O retículo endoplasmático rugoso está envolvido na síntese de proteínas, não na produção de energia.`;
+}
+
+
+
+
+
+
+function letraA2() {
+    const feedback = document.querySelector('.feedback2');
+    const containnerFeedback = document.querySelector('.containner-feedback2');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Produção de ATP: Errado. A produção de ATP ocorre nas mitocôndrias.`;
+}
+
+function letraB2() {
+    const feedback = document.querySelector('.feedback2');
+    const containnerFeedback = document.querySelector('.containner-feedback2');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Síntese de lipídios: Errado. A síntese de lipídios ocorre principalmente no retículo endoplasmático liso.`;
+}
+
+function letraC2() {
+    const feedback = document.querySelector('.feedback2');
+    const containnerFeedback = document.querySelector('.containner-feedback2');
+    
+    containnerFeedback.style.backgroundColor = '#6dffb6';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Modificação, empacotamento e distribuição de proteínas: Certo. O complexo de Golgi modifica, empacota e distribui proteínas e lipídios para diferentes destinos na célula.`;
+}
+
+function letraD2() {
+    const feedback = document.querySelector('.feedback2');
+    const containnerFeedback = document.querySelector('.containner-feedback2');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Degradação de macromoléculas: Errado. A degradação de macromoléculas ocorre nos lisossomos.`;
+}
+
+function letraE2() {
+    const feedback = document.querySelector('.feedback2');
+    const containnerFeedback = document.querySelector('.containner-feedback2');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Transporte de íons: Errado. O transporte de íons é uma função das membranas celulares e organelas especializadas como vacúolos.`;
+}
+
+
+
+
+
+
+function letraA3() {
+    const feedback = document.querySelector('.feedback3');
+    const containnerFeedback = document.querySelector('.containner-feedback3');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Mitocôndria: Errado. As mitocôndrias estão presentes em ambas as células vegetais e animais.`;
+}
+
+function letraB3() {
+    const feedback = document.querySelector('.feedback3');
+    const containnerFeedback = document.querySelector('.containner-feedback3');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Ribossomo: Errado. Os ribossomos estão presentes em ambas as células vegetais e animais.`;
+}
+
+function letraC3() {
+    const feedback = document.querySelector('.feedback3');
+    const containnerFeedback = document.querySelector('.containner-feedback3');
+    
+    containnerFeedback.style.backgroundColor = '#6dffb6';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Vacúolo central: Certo. O vacúolo central é uma grande organela encontrada em células vegetais, responsável por armazenamento e manutenção da turgidez celular.`;
+}
+
+function letraD3() {
+    const feedback = document.querySelector('.feedback3');
+    const containnerFeedback = document.querySelector('.containner-feedback3');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Membrana plasmática: Errado. A membrana plasmática está presente em ambas as células vegetais e animais.`;
+}
+
+function letraE3() {
+    const feedback = document.querySelector('.feedback3');
+    const containnerFeedback = document.querySelector('.containner-feedback3');
+    
+    containnerFeedback.style.backgroundColor = '#fa7f72';
+    containnerFeedback.style.color = 'var(--cor-de-fundo-1)';
+    
+    feedback.innerHTML = `Citoplasma: Errado. O citoplasma está presente em ambas as células vegetais e animais.`;
+}
